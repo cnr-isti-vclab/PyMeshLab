566ddb9b0e8fd3fd64d6a86b81b7b876e91e9de9
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-06 17:39:52 +0200

    added meshlab as submodule

---
+++ install/linux/build_meshlab.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd "$(dirname "$(realpath "$0")")"; #move to script directory
+
+DISTRIB_PATH=$(realpath ../../meshlab_libs)
+
+BUILD_PATH=$(realpath ../../build)
+
+mkdir $BUILD_PATH
+
+bash ../../meshlab/install/linux/linux_build.sh $BUILD_PATH
+
+rm -r $DISTRIB_PATH/*
+
+cp -ar $BUILD_PATH/distrib/lib $DISTRIB_PATH/
+cp -ar $BUILD_PATH/distrib/plugins $DISTRIB_PATH/
+
+rm -r $BUILD_PATH
