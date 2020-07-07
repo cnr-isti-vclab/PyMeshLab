ace03c678911d8af72b2b51c672c72fffbc5c3ba
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-07 16:56:49 +0200

    example import mesh working

--- PyMeshLab.py
+++ PyMeshLab.py
@@ -2,4 +2,8 @@ from PyMeshLab import PyMeshLab
 
 m = PyMeshLab.MeshDocument()
 
+m.load_mesh()
+
 print(m.size())
+
+print(m.number_vertices_selected_mesh())
--- src/PyMeshLab.cpp
+++ src/PyMeshLab.cpp
@@ -1,17 +1,40 @@
 #include <pybind11/pybind11.h>
 #include <meshmodel.h>
+#include <vcg/../wrap/io_trimesh/import_obj.h>
 
 namespace py = pybind11;
 
-int add(int i, int j) {
-	return i + j;
-}
-
 PYBIND11_MODULE(PyMeshLab, m) {
-	py::class_<MeshDocument>(m, "MeshDocument")
-			.def(py::init())
-			.def("size",
+	py::class_<MeshDocument> meshDocumentClass(m, "MeshDocument");
+
+	//empty constructor
+	meshDocumentClass.def(py::init());
+
+	//test
+	meshDocumentClass.def(
+				"load_mesh",
+				[](MeshDocument& m) {
+					m.addNewMesh("/home/alessandro/Drive/Research/3DMeshes/bunny.obj", "mesh");
+					vcg::tri::io::ImporterOBJ<CMeshO>::Info oi;
+					vcg::tri::io::ImporterOBJ<CMeshO>::Open(m.mm()->cm, "/home/alessandro/Drive/Research/3DMeshes/bunny.obj", oi);
+				});
+
+	//size method
+	meshDocumentClass.def("size",
 				[](const MeshDocument& m) {
 					return m.size();
 				});
+
+	//set_current_mesh method
+	meshDocumentClass.def("set_current_mesh",
+				[](MeshDocument& m, int new_curr_id) {
+					m.setCurrentMesh(new_curr_id);
+				});
+
+	meshDocumentClass.def(
+				"number_vertices_selected_mesh",
+				[](MeshDocument& m) {
+					return m.mm()->cm.vn;
+				});
+
 }
