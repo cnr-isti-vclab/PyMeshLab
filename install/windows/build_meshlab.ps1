0367fcb481133962a1a6bd27fbc360e83422c5cc
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-15 13:06:21 +0200

    first try build meshlab windows actions

---
+++ install/windows/build_meshlab.ps1
@@ -0,0 +1,27 @@
+#saving location where script has been runned
+$DIR = Get-Location
+
+$DISTRIB_PATH = Join-Path $PSScriptRoot ..\..\PyMeshLab
+
+$BUILD_PATH = Join-Path $PSScriptRoot ..\..\build
+
+cd $PSScriptRoot #move to script directory
+
+New-Item -ItemType Directory -Force -Path $BUILD_PATH
+
+.\..\..\meshlab\install\linux\windows_build.ps1 $BUILD_PATH
+.\..\..\meshlab\install\linux\windows_deploy.ps1 $BUILD_PATH\distrib
+
+New-Item -ItemType Directory -Force -Path $DISTRIB_PATH\lib\meshlab\plugins
+Copy-Item -Path $BUILD_PATH\distrib\*.dll -Destination $DISTRIB_PATH\lib\
+Copy-Item -Path $BUILD_PATH\distrib\*.exp -Destination $DISTRIB_PATH\lib\
+Copy-Item -Path $BUILD_PATH\distrib\*.lib -Destination $DISTRIB_PATH\lib\
+Copy-Item -Path $BUILD_PATH\distrib\*.lib -Destination $DISTRIB_PATH\lib\
+Copy-Item -Path $BUILD_PATH\distrib\lib\win32-msvc\*.lib -Destination $DISTRIB_PATH\lib\meshlab\
+Copy-Item -Path $BUILD_PATH\distrib\lib\meshlab-common.lib -Destination $DISTRIB_PATH\lib\
+Copy-Item -Path $BUILD_PATH\distrib\plugins\* -Destination $DISTRIB_PATH\lib\meshlab\plugins\
+
+rm -r $BUILD_PATH
+
+#going back to original location
+cd $DIR
