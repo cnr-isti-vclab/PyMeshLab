394f10b22f761b1c6a007d7ca70e7df48d90de0c
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-20 16:05:38 +0200

    try modifying system path

--- PyMeshLab/__init__.py
+++ PyMeshLab/__init__.py
@@ -1,7 +1,8 @@
-import sys,os
-this_path = os.path.abspath(os.path.dirname(__file__))
-print("---- argv: " + sys.argv[0])
-print("---- path: " + this_path)
-first_path = os.path.join(this_path, 'lib')
-sys.path.append(first_path)
-sys.path.append(os.path.join(first_path, 'meshlab'))
+import platform,os
+if platform.system=='Windows':
+	this_path = os.path.abspath(os.path.dirname(__file__))
+	first_path = os.path.join(this_path, 'lib')
+	second_path = os.path.join(first_path, 'meshlab')
+	os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
+	os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
+	print("---- path: " + os.environ['PATH'])
