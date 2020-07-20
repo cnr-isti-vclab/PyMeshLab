24f3c51dc12340741c0df32419f7f4c7005f5125
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-20 16:19:57 +0200

    test

--- PyMeshLab/__init__.py
+++ PyMeshLab/__init__.py
@@ -1,8 +1,9 @@
 import platform,os
-if platform.system()=='Windows':
-	this_path = os.path.abspath(os.path.dirname(__file__))
-	first_path = os.path.join(this_path, 'lib')
-	second_path = os.path.join(first_path, 'meshlab')
-	os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
-	os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
-	print("---- path: " + os.environ['PATH'])
+print("platform: " + platform.system())
+#if platform.system()=='Windows':
+this_path = os.path.abspath(os.path.dirname(__file__))
+first_path = os.path.join(this_path, 'lib')
+second_path = os.path.join(first_path, 'meshlab')
+os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
+os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
+print("---- path: " + os.environ['PATH'])
