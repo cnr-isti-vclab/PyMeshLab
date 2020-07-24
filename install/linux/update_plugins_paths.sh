67b43cf88b057ca2a4ce36e2336ada119398553e
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-24 15:58:34 +0200

    test ldd filter_sketchfab

--- install/linux/update_plugins_paths.sh
+++ install/linux/update_plugins_paths.sh
@@ -7,3 +7,5 @@ sudo apt-get install -y patchelf #todo remove
 PLUGINS_DIR='../../pymeshlab/lib/meshlab/plugins'
 
 patchelf --set-rpath '$ORIGIN/../..' $PLUGINS_DIR/*.so
+
+ldd $PLUGINS_DIR/libfilter_sketchfab.so
