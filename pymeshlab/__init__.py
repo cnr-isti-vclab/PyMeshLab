190160c29342b004eac3d416b3d650bac44d7f4e
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-24 15:24:26 +0200

    test

--- pymeshlab/__init__.py
+++ pymeshlab/__init__.py
@@ -1,10 +1,13 @@
-#import platform,os
-#this_path = os.path.abspath(os.path.dirname(__file__))
-#first_path = os.path.join(this_path, 'lib')
-#second_path = os.path.join(first_path, 'meshlab')
-#if platform.system()=='Windows':
-#	os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
-#	os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
-#else:
-#	os.environ['PATH'] = first_path + ':' + os.environ['PATH']
-#	os.environ['PATH'] = second_path + ':' + os.environ['PATH']
+import platform
+import os
+this_path = os.path.abspath(os.path.dirname(__file__))
+first_path = os.path.join(this_path, 'lib')
+second_path = os.path.join(first_path, 'meshlab')
+if platform.system() == 'Windows':
+	os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
+	os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
+else:
+	os.environ['PATH'] = first_path + ':' + os.environ['PATH']
+	os.environ['PATH'] = second_path + ':' + os.environ['PATH']
+	
+print("\n$$$$$$$PATH:" + os.environ['PATH'] + "\n")
