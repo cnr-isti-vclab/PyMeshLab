f1cc74601a94e9fd0a825538568467aa4faadfdc
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-24 14:55:05 +0200

    fix init for pymeshlab package

--- pymeshlab/__init__.py
+++ pymeshlab/__init__.py
@@ -1,9 +1,10 @@
 import platform,os
-#if platform.system()=='Windows':
 this_path = os.path.abspath(os.path.dirname(__file__))
 first_path = os.path.join(this_path, 'lib')
 second_path = os.path.join(first_path, 'meshlab')
-os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
-os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
-
-print(===========================TEST==============================)
+if platform.system()=='Windows':
+	os.environ['PATH'] = first_path + '\\;' + os.environ['PATH']
+	os.environ['PATH'] = second_path + '\\;' + os.environ['PATH']
+else:
+	os.environ['PATH'] = first_path + ':' + os.environ['PATH']
+	os.environ['PATH'] = second_path + ':' + os.environ['PATH']
