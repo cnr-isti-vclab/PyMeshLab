642c841543f6d34b8b12d9da24002eb6a26075e4
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-25 10:23:13 +0200

    setup that downloads pymeshlab package depending on os and python version

--- setup.py
+++ setup.py
@@ -1,9 +1,49 @@
 from setuptools import setup
+import platform
+import sys
+import os
+import urllib.request
+import zipfile
+
+# read the contents of README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+f = open("PYML_VERSION", "r")
+pymeshlabversion = f.read()
+
+osused = platform.system()
+if osused == 'Darwin':
+    osused = 'macOS'
+
+if (osused != 'Linux' and osused != 'Windows' and osused != 'macOS'):
+    raise Exception("Operative System not supported")
+
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 5):
+    raise Exception("Must be using Python >= 3.5")
+
+pythonversion = str(sys.version_info[0]) + '.' + str(sys.version_info[1])
+
+baseurl = 'https://github.com/cnr-isti-vclab/PyMeshLab/releases/download/v' + pymeshlabversion + '/'
+filename = 'PyMeshLab_' + osused + '_python' + pythonversion + '.zip'
+url = baseurl + filename
+
+print('Downloading ' + filename)
+urllib.request.urlretrieve(url, this_directory + '/' + filename)
+
+with zipfile.ZipFile(filename, 'r') as zip_ref:
+    zip_ref.extractall('pymeshlab/')
+
+os.remove(this_directory + '/' + filename)
 
 setup(
     name='pymeshlab',
     version='0.1',
     description='A Python interface to MeshLab',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/cnr-isti-vclab/PyMeshLab',
     author='Alessandro Muntoni, Paolo Cignoni',
     author_email='alessandro.muntoni@isti.cnr.it',
