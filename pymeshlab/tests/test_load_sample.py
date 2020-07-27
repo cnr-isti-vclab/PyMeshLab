fb8e678c2afbc1529671e2c61ac17ce9008b082b
alemuntoni <muntoni.alessandro@gmail.com>
2020-07-27 17:00:30 +0200

    other small renaming

--- pymeshlab/tests/test_load_sample.py
+++ pymeshlab/tests/test_load_sample.py
@@ -5,26 +5,26 @@ from . import samples_common
 def test_load_meshes():
     print('\n')
     base_path = samples_common.samples_absolute_path()
-    md = ml.MeshSet()
+    ms = ml.MeshSet()
 
-    md.load_mesh(base_path + "bone.ply")
+    ms.load_mesh(base_path + "bone.ply")
 
-    print(md.number_meshes())
+    print(ms.number_meshes())
 
-    md.load_mesh(base_path + "airplane.obj")
+    ms.load_mesh(base_path + "airplane.obj")
 
-    print(md.number_meshes())
+    print(ms.number_meshes())
 
-    assert md.number_meshes() == 2
+    assert ms.number_meshes() == 2
 
-    md.set_current_mesh(0)
+    ms.set_current_mesh(0)
 
-    print(md.current_mesh().vertex_number())
+    print(ms.current_mesh().vertex_number())
 
-    assert md.current_mesh().vertex_number() == 1872
+    assert ms.current_mesh().vertex_number() == 1872
 
-    md.set_current_mesh(1)
+    ms.set_current_mesh(1)
 
-    print(md.current_mesh().vertex_number())
+    print(ms.current_mesh().vertex_number())
 
-    assert md.current_mesh().vertex_number() == 7017
+    assert ms.current_mesh().vertex_number() == 7017
--- pymeshlab/tests/test_save_sample.py
+++ pymeshlab/tests/test_save_sample.py
@@ -6,8 +6,8 @@ def test_save_mesh():
     print('\n')
     base_path = samples_common.samples_absolute_path()
     output_path = samples_common.test_output_path()
-    md = ml.MeshSet()
+    ms = ml.MeshSet()
 
-    md.load_mesh(base_path + "bone.ply")
+    ms.load_mesh(base_path + "bone.ply")
 
-    md.save_mesh(output_path + "bone_saved.obj")
+    ms.save_mesh(output_path + "bone_saved.obj")
