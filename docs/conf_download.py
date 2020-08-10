835e4938f173d14cc6666e8b84c09be0bc936efe
alemuntoni <muntoni.alessandro@gmail.com>
2020-08-10 21:15:37 +0200

    fix prerelase

--- docs/conf_download.py
+++ docs/conf_download.py
@@ -26,11 +26,67 @@ url = baseurl + filename
 
 
 print('Downloading ' + filename)
-urllib.request.urlretrieve(url, this_directory + '/' + filename)
+urllib.request.urlretrieve(url, './' + filename)
 
 with zipfile.ZipFile(filename, 'r') as zip_ref:
     zip_ref.extractall('../pymeshlab/')
 
-os.remove(this_directory + '/' + filename)
+os.remove(filename)
 
-os.system("conf.py")
+os.system("python3 conf.py")
+
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+sys.path.insert(0, os.path.abspath('../pymeshlab'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'PyMeshLab'
+copyright = '2020, Alessandro Muntoni, Paolo Cignoni'
+author = 'Alessandro Muntoni, Paolo Cignoni'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [ 
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.napoleon',
+    'readthedocs_ext.readthedocs'
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
