973f36763e93fdd5610dc9f00ac318c116332035
alemuntoni <muntoni.alessandro@gmail.com>
2020-11-04 11:59:55 +0100

    LINUX CMAKE WORKING - cleanups

--- scripts/Linux/qmake/update_plugins_paths.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd "$(dirname "$(realpath "$0")")"; #move to script directory
-
-sudo apt-get install -y patchelf #todo remove
-
-PLUGINS_DIR='../../pymeshlab/lib/meshlab/plugins'
-
-patchelf --set-rpath '$ORIGIN/../..' $PLUGINS_DIR/*.so
--- scripts/simple_test.py
+++
@@ -1,7 +0,0 @@
-import pymeshlab
-
-ms = pymeshlab.MeshSet(verbose=True)
-
-ms.print_status()
-
-ms.print_plugin_list()
