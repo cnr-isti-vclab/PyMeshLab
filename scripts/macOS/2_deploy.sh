01275cce9614d6b3824a7f943ba2863b6316536d
alemuntoni <muntoni.alessandro@gmail.com>
2020-11-05 14:41:01 +0100

    first macos build with cmake

---
+++ scripts/macOS/2_deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# This is a script shell for deploying a pymeshlab-portable folder.
+# Requires a properly built PyMeshLab (see 1_build.sh).
+#
+# This script can be run only in the oldest supported linux distro
+# due to linuxdeployqt tool choice (see https://github.com/probonopd/linuxdeployqt/issues/340).
+#
+# Without given arguments, the folder that will be deployed is pymeshlab, which
+# should be the path where PyMeshLab has been installed (default output of 1_build.sh).
+#
+# You can give as argument the path where you installed PyMeshLab.
+
+SCRIPTS_PATH=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+LINUXDEPLOYQT_PATH=$SCRIPTS_PATH/../../src/meshlab/scripts/Linux/resources
+
+#checking for parameters
+if [ "$#" -eq 0 ]
+then
+    BUNDLE_PATH=$SCRIPTS_PATH/../../pymeshlab
+else
+    BUNDLE_PATH=$(realpath $1)
+fi
