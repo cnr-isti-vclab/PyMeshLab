46a8f46d277af70d5310ca1158879956999a25ad
alemuntoni <muntoni.alessandro@gmail.com>
2021-01-29 13:01:30 +0100

    fix example default value parameters

--- pymeshlab/tests/example_apply_filter_parameters_check_default_values.py
+++ pymeshlab/tests/example_apply_filter_parameters_check_default_values.py
@@ -5,12 +5,12 @@ def example_apply_filter_parameters_check_default_values():
     # lines needed to run this specific example
     print('\n')
     from . import samples_common
-    output_path = samples_common.test_output_path()
+    base_path = samples_common.samples_absolute_path()
 
     # create a new MeshSet
     ms = pymeshlab.MeshSet()
 
-    ms.load_new_mesh(output_path + 'colored_airplane.ply')
+    ms.load_new_mesh(base_path + 'colored_airplane.ply')
 
     # returns a dictionary which binds, for each parameter (a string with its name) the
     # default value that would be used if the given filter would be applied at the current
