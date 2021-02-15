95fc34feb81c86166b1cd41666883932545df2a8
alemuntoni <muntoni.alessandro@gmail.com>
2021-02-15 12:25:29 +0100

    fix last commit

--- pymeshlab/__init__.py
+++ pymeshlab/__init__.py
@@ -1,7 +1,7 @@
 from .pmeshlab import *
 try:
     pmeshlab.load_default_plugins()
-except pmeshlab.PyMeshLabException as e:
+except PyMeshLabException as e:
     print("Warning: " + e)
 
 pmeshlab.print_pymeshlab_version()
