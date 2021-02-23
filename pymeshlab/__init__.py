b4d2a6ac8aa54ecce4e8eef42f420ac191748f39
alemuntoni <muntoni.alessandro@gmail.com>
2021-02-23 14:21:45 +0100

    try fix init for linux

--- pymeshlab/__init__.py
+++ pymeshlab/__init__.py
@@ -1,9 +1,13 @@
 from .pmeshlab import *
+from sys import platform
 import os
 import pathlib
 
 this_path=str(pathlib.Path(__file__).parent.absolute())
-os.environ['QT_PLUGIN_PATH']=this_path
+if platform == 'win32':
+    os.environ['QT_PLUGIN_PATH']=this_path
+elif os.name == "linux":
+    os.environ['QT_PLUGIN_PATH']=this_path+'/lib'
 
 try:
     load_default_plugins()
@@ -13,13 +17,13 @@ except PyMeshLabException as e:
 print_pymeshlab_version()
 
 def use_cpu_opengl():
-    if os.name == 'nt':
+    if platform == 'win32':
         os.rename(this_path + '\\opengl32sw.dll', this_path + '\\opengl32.dll')
     else:
         print("Nothing to do.")
 
 def do_not_use_cpu_opengl():
-    if os.name == 'nt':
+    if platform == 'win32':
         os.rename(this_path + '\\opengl32.dll', this_path + '\\opengl32sw.dll')
     else:
         print("Nothing to do.")
