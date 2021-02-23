7d8eec30290dc297a94548c04ae422fcfeb7cff5
alemuntoni <muntoni.alessandro@gmail.com>
2021-02-23 12:46:26 +0100

    use_cpu_opengl on windows tests

--- pymeshlab/__init__.py
+++ pymeshlab/__init__.py
@@ -2,7 +2,8 @@ from .pmeshlab import *
 import os
 import pathlib
 
-os.environ['QT_PLUGIN_PATH']=str(pathlib.Path(__file__).parent.absolute())
+this_path=str(pathlib.Path(__file__).parent.absolute())
+os.environ['QT_PLUGIN_PATH']=this_path
 
 try:
     load_default_plugins()
@@ -10,3 +11,15 @@ except PyMeshLabException as e:
     print("Warning:\n" + str(e))
 
 print_pymeshlab_version()
+
+def use_cpu_opengl():
+    if os.name == 'nt':
+        os.rename(this_path + '/opengl32sw.dll', this_path + '/opengl32.dll')
+    else:
+        print("Nothing to do.")
+
+def do_not_use_cpu_opengl():
+    if os.name == 'nt':
+        os.rename(this_path + '/opengl32.dll', this_path + '/opengl32sw.dll')
+    else:
+        print("Nothing to do.")
