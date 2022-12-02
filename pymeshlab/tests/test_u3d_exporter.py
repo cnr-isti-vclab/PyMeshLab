b8b1471c6f07b4cd9660c9b713ef01ba549072a6
alemuntoni <muntoni.alessandro@gmail.com>
2022-12-02 17:58:18 +0100

    test u3d exporter

---
+++ pymeshlab/tests/test_u3d_exporter.py
@@ -0,0 +1,16 @@
+import pymeshlab as ml
+from . import samples_common
+
+import pytest
+from sys import platform
+
+
+def test_ambient_occlusion():
+    print('\n')
+    base_path = samples_common.samples_absolute_path()
+    output_path = samples_common.test_output_path()
+    ms = ml.MeshSet()
+
+    ms.load_new_mesh(base_path + "bunny.obj")
+
+    ms.save_current_mesh(output_path + "bunny.u3d")
